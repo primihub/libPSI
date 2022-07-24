213037d4aa0db4d257937d2d953f1b965e4a5234
yankaili2006 <yankaili2006@gmail.com>
2022-07-25 07:27:51 +0800

    Update config.h

--- config.h
+++ config.h
@@ -8,13 +8,13 @@
 /* #define ENABLE_GRR_PSI  1 */
 
 // build the library with RR16 PSI enabled
-#define ENABLE_RR16_PSI  1
+// #define ENABLE_RR16_PSI  1
 
 // build the library with RR17 PSI enabled
-#define ENABLE_RR17_PSI  1
+// #define ENABLE_RR17_PSI  1
 
 // build the library with RR17 PSI enabled
-#define ENABLE_RR17B_PSI  1
+// #define ENABLE_RR17B_PSI  1
 
 // build the library with KKRT PSI enabled
 #define ENABLE_KKRT_PSI  1
@@ -23,10 +23,10 @@
 #define ENABLE_ECDH_PSI  1
 
 // build the library with DRRN PSI enabled
-#define ENABLE_DRRN_PSI  1
+// #define ENABLE_DRRN_PSI  1
 
 // build the library with PRTY PSI enabled
-#define ENABLE_PRTY_PSI  1
+// #define ENABLE_PRTY_PSI  1
 
 // #define ENABLE_OOS 1
 // #define ENABLE_KKRT 1
